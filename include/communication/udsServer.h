eec7de1917f7aef2bbb544ca9ba757a6b050b330
Olaf <olaf.chojnacki04@wp.pl>
2026-03-28 16:53:18 +0100

    Limit ratio of socket requests, to avoid spaming,

--- include/communication/udsServer.h
+++ include/communication/udsServer.h
@@ -5,10 +5,11 @@
 #include <atomic>
 #include <string>
 #include <thread>
+#include <chrono>
 
 class UdsServer {
 public:
-    UdsServer(std::string socketPath, ISnapshotProvider& provider);
+    UdsServer(std::string socketPath, ISnapshotProvider& provider, std::chrono::milliseconds minInterval = std::chrono::milliseconds(250));
     ~UdsServer();
 
     void start();
@@ -24,4 +25,6 @@ private:
     std::thread worker_;
     std::atomic<bool> running_{false};
     int serverFd_{-1};
+    std::chrono::steady_clock::time_point lastSendTime_{};
+    std::chrono::milliseconds minInterval_;
 };
